0c792fd6b03460cc3ae91ecc68f1229901533854
unknown <c:\users\workstation\appdata\roaming\the bat!>
2022-01-28 14:11:46 +0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,58 @@
+public class main {
+
+    private int Human;
+    private String age;
+    private String sex;
+    private String name;
+    private String lastName;
+
+    public main(int human, String age, String sex, String name, String lastName) {
+        Human = human;
+        this.age = age;
+        this.sex = sex;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+
+
+
+
+    public void setHuman(int human) {Human = human; }
+
+    public int getHuman() {
+        return Human;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
